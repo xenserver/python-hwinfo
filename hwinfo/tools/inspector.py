a981767b409b1d5125c2ac2875ce745dfb76b2b9
alexhimmel <alex.ac@qq.com>
2023-04-10 04:04:31 -0400

    CP-41972: update hwinfo to support python3 ACK and fixed some problems
    Signed-off-by: alexhimmel <alex.ac@qq.com>

--- hwinfo/tools/inspector.py
+++ hwinfo/tools/inspector.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from argparse import ArgumentParser
 import paramiko
@@ -36,13 +36,13 @@ def remote_command(client, cmd):
 
 def local_command(cmd):
     cmdstr = ' '.join(cmd)
-    process = subprocess.Popen(cmdstr, stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(cmdstr, stdout=subprocess.PIPE, shell=True, universal_newlines=True)
     stdout, stderr = process.communicate()
     if process.returncode == 0:
-        return str(stdout).strip()
+        return stdout.strip()
     else:
-        print "RC: %s" % process.returncode
-        print stdout
+        print("RC: %s" % process.returncode)
+        print(stdout)
         raise Exception("stderr: %s" % str(stderr))
 
 def find_in_tarball(tarball, filename):
@@ -160,7 +160,7 @@ class Host(object):
 
         try:
             os_rec = self.get_os_info()
-            for k, v in os_rec.iteritems():
+            for k, v in os_rec.items():
                 rec[k] = v
         except Exception:
             #Ignore failures. Only supports XS right now.
@@ -198,7 +198,7 @@ def combine_recs(rec_list, key):
     for rec in rec_list:
         rec_key = rec[key]
         if rec_key in final_recs:
-            for k, v in rec.iteritems():
+            for k, v in rec.items():
                 if k in final_recs[rec_key] and final_recs[rec_key][k] != v:
                     raise Exception("Mis-match for key '%s'" % k)
                 final_recs[rec_key][k] = v
@@ -234,7 +234,7 @@ class HostFromLogs(Host):
             return devs
         except FileNotFound:
             # Fall back to looking for the file lspci-vv.out
-            print "***lspci-nnm.out found. Falling back to looking for lspci-vv.out and lspci-n.out.***"
+            print("***lspci-nnm.out found. Falling back to looking for lspci-vv.out and lspci-n.out.***")
             lspci_vv_recs = parse_data(LspciVVParser, self._load_from_file('lspci-vv.out'))
             lspci_n_recs = parse_data(LspciNParser, self._load_from_file('lspci-n.out'))
             all_recs = lspci_vv_recs + lspci_n_recs
@@ -302,7 +302,7 @@ def rec_to_table(rec):
     table = PrettyTable(["Key", "Value"])
     table.align['Key'] = 'l'
     table.align['Value'] = 'l'
-    for k, v in rec.iteritems():
+    for k, v in rec.items():
         table.add_row([k, v])
     return table
 
@@ -345,7 +345,7 @@ def create_unit(title, content):
 def validate_args(args):
     if args.machine != 'localhost':
         if not args.username or not args.password:
-            print "Error: you must specify a username and password to query a remote machine."
+            print("Error: you must specify a username and password to query a remote machine.")
             sys.exit(1)
 
 def system_info(host, options):
@@ -429,6 +429,6 @@ def main():
         options = filter_choices
 
     if args.export:
-        print export_system_info(host, options)
+        print(export_system_info(host, options))
     else:
-        print system_info(host, options)
+        print(system_info(host, options))
